8c1727946b3e6c45fe3527dd82d7350b79d960a8
eric <yuankaiz@yonyou.com>
2024-05-16 10:58:49 +0800

    welcome

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import reactIcon from '@/assets/react.svg'
 const App = () => {
-    return <div> hello! young fella~~~</div>
+    return (
+        <div className="flex flex-col justify-center items-center w-full h-full bg-sky-100">
+            <img src={reactIcon} alt="icon" className="w-1/12 animate-spin" />
+            <p className="pt-10 text-lg text-gray-600 md:!text-2xl">{"Let's do it "}🔨</p>
+        </div>
+    )
 }
 
 export default App
