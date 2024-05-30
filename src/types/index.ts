4e64b5e5f880bdd6b9ae43e66ad07b68128cbfc0
eric <yuankaiz@yonyou.com>
2024-05-30 11:27:57 +0800

    feat: change CustomRouteFields.meta value type

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,7 +3,7 @@ import type { IndexRouteObject, NonIndexRouteObject } from 'react-router-dom'
 // custom route type
 interface CustomRouteFields {
     meta?: {
-        [key: string]: string
+        [key: string]: any
     }
 }
 type AppIndexRouteObject = IndexRouteObject & CustomRouteFields
