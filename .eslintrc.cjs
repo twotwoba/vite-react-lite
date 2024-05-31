b3bf94aa7aa410744d3a5dfaee9a3265112f714e
eric <yuankaiz@yonyou.com>
2024-05-31 10:33:04 +0800

    change eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,9 +23,14 @@ module.exports = {
     plugins: ['react-refresh'],
     rules: {
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unsafe-return': 'off'
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-misused-promises': 'off'
     },
     settings: {
         react: {
