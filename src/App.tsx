7519d1023e6afb03e6634238cb0277af4db30006
eric <yuankaiz@yonyou.com>
2024-07-09 16:36:25 +0800

    set default fontSize 62.5%->1rem->10px,& tw rootFontSize 10

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ const App = () => {
     return (
         <div className="flex flex-col justify-center items-center w-full h-full bg-sky-100">
             <img src={reactIcon} alt="icon" className="w-1/12 animate-spin" />
-            <p className="pt-10 text-lg text-gray-600 md:!text-2xl">{"Let's do it "}🔨</p>
+            <p className="px-6 text-lg text-gray-600 md:!text-2xl">{"Let's do it "}🔨</p>
         </div>
     )
 }
